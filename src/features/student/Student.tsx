9aca3655dbdeb5d96e2265772f35531fbcdcbd4a
Gefen hurvitz <gefenhurvitz@Gefens-MacBook-Air.local>
2023-02-12 07:52:11 +0200

    new redux ts project with student

---
+++ src/features/student/Student.tsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+
+import { useAppSelector, useAppDispatch } from '../../app/hooks';
+import {
+  decrement,
+  increment,
+  incrementByAmount,
+  incrementAsync,
+  incrementIfOdd,
+  selectCount,
+} from './studentSlice';
+import styles from './Counter.module.css';
+
+export function Counter() {
+  const count = useAppSelector(selectCount);
+  const dispatch = useAppDispatch();
+  const [incrementAmount, setIncrementAmount] = useState('2');
+
+  const incrementValue = Number(incrementAmount) || 0;
+
+  return (
+    <div>
+
+    </div>
+  );
+}
